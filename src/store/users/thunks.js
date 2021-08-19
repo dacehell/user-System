5acbc0362cf323635fc41ea27e946a3d762f80c6
dacehell <caldera.david78@gmail.com>
2021-08-19 17:01:26 -0400

    fix method DELETE  in thunks api

--- src/store/users/thunks.js
+++ src/store/users/thunks.js
@@ -32,11 +32,12 @@ export const deleteUserStartThunk = (id) => {
   return async (dispatch, getState) => {
     dispatch(deleteUserStart());
     try {
+      const requestOptions = {
+        method: "DELETE",
+      };
       const response = await fetch(
         `https://jsonplaceholder.typicode.com/users/${id}`,
-        {
-          method: "DELETE",
-        }
+        requestOptions
       );
       const data = await response.json;
       dispatch(deleteUserSuccess(id));
